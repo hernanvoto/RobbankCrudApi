22a51ea3abe9882bcd1037bac2de1826aa4330e6
hernanvoto <rob.voto.au@gmail.com>
2024-04-03 09:31:10 +1100

    Initial commit

---
+++ src/main/java/com/airconsole/reservation/RobbankCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.airconsole.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobbankCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobbankCrudApiApplication.class, args);
+	}
+
+}
